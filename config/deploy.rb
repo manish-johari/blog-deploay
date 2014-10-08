3d4c2db8bf94f4a5d7617902e77fb9cdf24b1ef4
Manish Johari <manish.johari@kiwitech.com>
2014-10-08 10:37:07 +0530

    deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,63 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# # Deployment server info
+# set :application, "blog-deploay"      # "APP NAME"
+
+# # SCM info
+# set :repository,  "git@github.com:manish-johari/blog-deploay.git"  # "GIT REMOTE REPO URL"
+# #set :scm,         :git
+
+# set :deploy_to, "/home/kiwitech/rails_projects/self_learning"   # the destination dir
+
+
+
+# # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# set :branch, '2_setup_new_project_on_gitHub'
+
+# set :use_sudo, false
+
+# set :rails_env, 'development'
+
+# # General config stuff
+# set :keep_releases, 4
+
+# role :web, '127.0.0.1'          # Your HTTP server, Apache/etc
+# role :app, '127.0.0.1'          # This may be the same as your `Web` server
+# #role :db,  "127.0.0.1", :primary => true # This is where Rails migrations will run
+# role :db, '127.0.0.1'
+
+# set :scm, :git
+
+# #set :deploy_via, :copy
+
+
+# # if you want to clean up old releases on each deploy uncomment this:
+# # after "deploy:restart", "deploy:cleanup"
+
+# # if you're still using the script/reaper helper you will need
+# # these http://github.com/rails/irs_process_scripts
+
+# # If you are using Passenger mod_rails uncomment this:
+# # namespace :deploy do
+# #   task :start do ; end
+# #   task :stop do ; end
+# #   task :restart, :roles => :app, :except => { :no_release => true } do
+# #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+# #   end
+# # end
+
+set :application, "blog-deploay"
+set :repository,  "git@github.com:manish-johari/blog-deploay.git"
+
+set :branch, '2_setup_new_project_on_gitHub'
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "localhost"                          # Your HTTP server, Apache/etc
+role :app, "localhost"                          # This may be the same as your `Web` server
+role :db,  "localhost", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :use_sudo, false
+set :deploy_to, "/home/kiwitech/rails_projects/self_learning"
+set :keep_releases, 4
